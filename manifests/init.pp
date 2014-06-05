c1f3e6d05db027a43df0785c2eaaf7fe159b5ca7
bhiles <bennetthiles@gmail.com>
2014-06-05 15:51:17 -0700

    ensure present

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,15 +5,8 @@
 #   include toggl
 #
 class toggl {
-  if file('/var/db/.puppet_appdmg_installed_Toggl', '/dev/null') =~ /https:\/\/download\.toggl\.com\/toggldesktop\/latest\/TogglDesktopInstaller_Release\.dmg/ {
-    notice('Detected an old version of puppet-toggl (1.0.2 or older). The appdmg cookie will be removed so that the latest version of ToggleDesktop.app can be installed.')
-    file {'/var/db/.puppet_appdmg_installed_Toggl':
-      ensure => 'absent',
-      before => Package['Toggl']
-    }
-  }
   package { 'Toggl':
-    ensure   => 'present',
+    ensure   => present,
     provider => 'appdmg',
     source   => 'https://toggl.com/api/v8/installer?app=td&platform=darwin&channel=stable'
   }
