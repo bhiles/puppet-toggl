582fc8db3f27b1e4c3d7ebd66e82512f79584189
bhiles <bennetthiles@gmail.com>
2014-06-05 14:23:09 -0700

    updated toggl download location and added spec/module symlink

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,9 +4,6 @@
 #
 #   include toggl
 #
-# TODO: switch source to the following, pending release of puppetlabs/puppet#2249
-#   source   => 'https://www.toggl.com/api/v8/installer?app=td&platform=darwin&channel=stable'
-#
 class toggl {
   if file('/var/db/.puppet_appdmg_installed_Toggl', '/dev/null') =~ /https:\/\/download\.toggl\.com\/toggldesktop\/latest\/TogglDesktopInstaller_Release\.dmg/ {
     notice('Detected an old version of puppet-toggl (1.0.2 or older). The appdmg cookie will be removed so that the latest version of ToggleDesktop.app can be installed.')
@@ -17,6 +14,6 @@ class toggl {
   }
   package { 'Toggl':
     provider => 'appdmg',
-    source   => 'http://assets.toggl.com/installers/TogglDesktop-2014-03-06-16-37-57.dmg'
+    source   => 'https://toggl.com/api/v8/installer?app=td&platform=darwin&channel=stable'
   }
 }
--- spec/classes/toggl_spec.rb
+++ spec/classes/toggl_spec.rb
@@ -4,7 +4,7 @@ describe 'toggl' do
   it do
     should contain_package('Toggl').with({
       :provider => 'appdmg',
-      :source   => 'http://assets.toggl.com/installers/TogglDesktop-2014-03-06-16-37-57.dmg',
+      :source   => 'https://toggl.com/api/v8/installer?app=td&platform=darwin&channel=stable',
     })
   end
 end
