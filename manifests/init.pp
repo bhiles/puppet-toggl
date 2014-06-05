ebff632b3899cf3df4241168b100ac5837697ed5
bhiles <bennetthiles@gmail.com>
2014-06-05 15:44:45 -0700

    include ensure state

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,6 +13,7 @@ class toggl {
     }
   }
   package { 'Toggl':
+    ensure   => 'present',
     provider => 'appdmg',
     source   => 'https://toggl.com/api/v8/installer?app=td&platform=darwin&channel=stable'
   }
